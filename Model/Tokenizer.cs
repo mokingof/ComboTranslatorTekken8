2b235a956271a670c3b13145c66aeb7417253d36
Abdill <dmohammed@hotmail.co.uk>
2024-07-26 16:31:24 +0100

    Ive setup the Tokenzier. It super basic but I just wanted to get familiar with it, next is complex buttons

--- Model/Tokenizer.cs
+++ Model/Tokenizer.cs
@@ -1,6 +1,40 @@
-﻿namespace ComboTranslatorTekken8.Model
+﻿using System.Text.RegularExpressions;
+
+namespace ComboTranslatorTekken8.Model
 {
     public class Tokenizer
     {
+        Regex Button = new Regex(@"[1-4]");
+        Regex Direction = new Regex(@"[1-4]");
+        
+
+
+
+        public List<Token> TokenizeString(string input)
+        {
+            List<Token> Tokens = new();
+            var currentPosition = 0;
+
+            while (currentPosition < input.Length)
+            {
+                if (char.IsDigit(input[currentPosition]))
+                {
+                    string value = FoundNumber(input[currentPosition]);
+                    Tokens.Add(new(TokenType.Button, value, currentPosition));
+                    currentPosition++;
+                }
+
+
+                currentPosition++;
+            }
+            return Tokens;
+        }
+        private string FoundNumber(char c)
+        {
+            Match match = Button.Match(c.ToString());
+            return match.Value;   
+            
+        }
+
     }
 }
--- obj/Debug/net8.0/ComboTranslatorTekken8.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComboTranslatorTekken8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComboTranslatorTekken8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa0e1dabf8dc148887e5d9f60dbe4a336e31eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2055ca807a2b8cfbb04a64f273db0c73176900e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComboTranslatorTekken8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComboTranslatorTekken8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
