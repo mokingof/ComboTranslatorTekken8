41d9c13a2402361af3de11f5ed6d1918ea46a4fd
Abdill <dmohammed@hotmail.co.uk>
2024-07-27 13:14:52 +0100

    used regex seperate regex for single and multi button as I think it would be better. It looks correct to me and each button gets added to my Tokens list. Next is simple directions

--- Model/Tokenizer.cs
+++ Model/Tokenizer.cs
@@ -4,37 +4,77 @@ namespace ComboTranslatorTekken8.Model
 {
     public class Tokenizer
     {
-        Regex Button = new Regex(@"[1-4]");
-        Regex Direction = new Regex(@"[1-4]");
+        //PrintCombo("b3,dash,2+3+1,ff4,ssl,UB1,d/f1+2+3");
+        Regex SingleButton = new Regex(@"[1234]");
+        Regex MultiButton = new Regex(@"^[1-4](\+[1-4]){1,3}$");
         
-
 
 
         public List<Token> TokenizeString(string input)
         {
             List<Token> Tokens = new();
-            var currentPosition = 0;
 
-            while (currentPosition < input.Length)
+            char[] delimiters = { ',' };  
+            List<string> temp = input.Split(delimiters).ToList();
+
+            for (int i = 0; i < temp.Count; i++)
             {
-                if (char.IsDigit(input[currentPosition]))
+
+                if (temp[i].Any(char.IsDigit))
                 {
-                    string value = FoundNumber(input[currentPosition]);
-                    Tokens.Add(new(TokenType.Button, value, currentPosition));
-                    currentPosition++;
+                    if (i + 1 < temp.Count && temp[i + 1].Equals("+"))
+                    {
+                        var combination = CombinationButtons(temp[i]);
+                        Tokens.Add(new Token(TokenType.CombinedButton, combination, i));
+
+                    } 
+                    else
+                    {
+                        var button = Button(input[i]);
+                        Tokens.Add(new Token(TokenType.Button, button, i));
+                    }
                 }
+                
+                
 
-
-                currentPosition++;
             }
             return Tokens;
         }
-        private string FoundNumber(char c)
+        private string Button(char input)
         {
-            Match match = Button.Match(c.ToString());
-            return match.Value;   
-            
+            Match match = SingleButton.Match(input.ToString());
+            return match.Value;
+        }
+        private string CombinationButtons(string input)
+        {   
+            if (ContainsDuplicate(input))
+            {
+                throw new Exception("Error: Duplicate button input");
+            }
+            Match match = MultiButton.Match(input.ToString());
+            return match.Value;
+          
         }
 
+        private string Directions(string input)
+        {
+            return null;
+
+        }
+        private string SpecialCommands(string input)
+        {
+            return null;
+        }
+        private string Separators(string input)
+        {
+            return null;
+        }
+
+        private bool ContainsDuplicate(string input)
+        {
+            return input.Distinct().Count() == input.Length;
+           
+        
+        }
     }
 }
